6081d1c08bc176dc7876365d49fee2e8bed97414
lobomanz <lovro.monkey@gmail.com>
2024-12-01 15:33:51 +0100

    changes

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import People from '@/views/People.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  // mode:'hash',
   routes: [
     {
       path: '/',
