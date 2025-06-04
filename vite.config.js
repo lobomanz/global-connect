09d6038938d072aea7e4371c2368b946664a519f
Lovro Klaric <lovro.klaric@binom.hr>
2025-06-04 15:45:22 +0200

    vite config diff

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,34 @@
+// import { fileURLToPath, URL } from 'node:url'
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// export default defineConfig(({ command }) => ({
+//   base: command === 'build' ? '/vueapp/' : '/', // ✅ Only use base in build
+//   plugins: [
+//     vue(),
+//   ],
+//   css: {
+//     preprocessorOptions: {
+//       scss: {
+//         additionalData: `@import "@/assets/styles/theme.scss";`
+//       }
+//     }
+//   },
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// }))
+
+
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/vueapp/' : '/', // ✅ Only use base in build
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [
     vue(),
   ],
@@ -19,4 +44,4 @@ export default defineConfig(({ command }) => ({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-}))
+})
